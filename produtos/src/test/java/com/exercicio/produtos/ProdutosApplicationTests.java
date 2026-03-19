797665dbfea2c971faa3c2b03e6a0cf97638a94a
pedrolcmendes <pepelmendes05@gmail.com>
2026-03-19 12:35:23 -0300

    primeiro commit

---
+++ produtos/src/test/java/com/exercicio/produtos/ProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercicio.produtos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
